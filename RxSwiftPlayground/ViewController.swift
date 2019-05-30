816eeeb3f39da0a232f693212a3d958295cb812d
Raphael Berendes <raphael-berendes@gmx.de>
2019-05-30 17:06:02 +0200

    Implement first example

--- RxSwiftPlayground/ViewController.swift
+++ RxSwiftPlayground/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import RxCocoa
+import RxSwift
 
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var firstNameTextField: UITextField!
+    @IBOutlet private weak var lastNameTextField: UITextField!
+    @IBOutlet private weak var greetingsLabel: UITextField!
+    
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        doObservableStuff()
     }
-
-
+    
+    private func doObservableStuff() {
+        
+        Observable.combineLatest(firstNameTextField.rx.text.orEmpty, lastNameTextField.rx.text.orEmpty) { $0 + " " + $1 }
+            .map { "Moin, \($0)" }
+            .bind(to: greetingsLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
 }
-
